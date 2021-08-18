3bdb51faaba8f22e3e894093223ab141c231913b
2105624 <2105624@students.wits.ac.za>
2021-08-18 11:23:09 +0200

    completed edit profile

--- app/src/main/java/com/example/witsonline/EditProfile.java
+++ app/src/main/java/com/example/witsonline/EditProfile.java
@@ -21,8 +21,13 @@ import androidx.core.util.PatternsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputLayout;
@@ -32,6 +37,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfile extends AppCompatActivity implements View.OnScrollChangeListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -51,15 +58,18 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
 
     //boolean for password update
     boolean passwordUpdateRequired = false;
-    String passwordUpdate;
+    private String passwordUpdate;
 
     //This is for the delay while loading the email
-    ProgressBar progressBar;
-    RelativeLayout relativeLayout;
+    private ProgressBar progressBar;
+    private RelativeLayout relativeLayout;
 
     //URLs for php
-    String studURL = "https://lamp.ms.wits.ac.za/home/s2105624/getStudentProfile.php?unum=";
-    String instURL = "https://lamp.ms.wits.ac.za/home/s2105624/getInstructorProfile.php?unum=";
+    private String studURL = "https://lamp.ms.wits.ac.za/home/s2105624/getStudentProfile.php?unum=";
+    private String instURL = "https://lamp.ms.wits.ac.za/home/s2105624/getInstructorProfile.php?unum=";
+    private String studUpdateURL = "https://lamp.ms.wits.ac.za/home/s2105624/studentProfileUpdate.php";
+    private String instUpdateURL = "https://lamp.ms.wits.ac.za/home/s2105624/instructorProfileUpdate.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,12 +104,11 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
             @Override
             @Generated
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (passwordSwitch.isChecked()){
+                if (passwordSwitch.isChecked()) {
                     password.setVisibility(View.VISIBLE);
                     confirmPass.setVisibility(View.VISIBLE);
                     passwordUpdateRequired = true;
-                }
-                else{
+                } else {
                     password.getEditText().setText("");
                     confirmPass.getEditText().setText("");
                     password.setError(null);
@@ -138,30 +147,93 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
             @Generated
             public void onClick(View v) {
                 boolean valid = true;
-                if (passwordUpdateRequired){
-                    passwordUpdate = "password";
+                if (passwordUpdateRequired) {
+                    passwordUpdate = "pass";
                     //validate with password text inputs
-                    if (isEmpty(firstName) | isEmpty(lastName) | validateEmail(email) | !validatePassword(password,confirmPass)){
+                    if (isEmpty(firstName) | isEmpty(lastName) | !validEmail(email) | !validPassword(password, confirmPass)) {
                         valid = false;
                     }
-                }
-                else{
-                    passwordUpdate = "nopassword";
+                    else{
+                        valid = true;
+                    }
+                } else {
+                    passwordUpdate = "nopass";
                     //validate without password text inputs
-                    if (isEmpty(firstName) | isEmpty(lastName) | validateEmail(email)){
+                    if (isEmpty(firstName) | isEmpty(lastName) | !validEmail(email)) {
                         valid = false;
+                    }
+                    else{
+                        valid = true;
                     }
                 }
 
-                if (valid){
+                if (valid) {
                     //perfom the update
-
+                    if (USER.STUDENT) {
+                        //update student
+                        StringRequest request = new StringRequest(Request.Method.POST, studUpdateURL, new Response.Listener<String>() {
+                            @Override
+                            @Generated
+                            public void onResponse(String response) {
+                                System.out.println(response);
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            @Generated
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println(error.getMessage());
+                            }
+                        }) {
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> parameters = new HashMap<>();
+                                parameters.put("unum", USER.USER_NUM);
+                                parameters.put("fname", firstName.getEditText().getText().toString());
+                                parameters.put("lname", lastName.getEditText().getText().toString());
+                                parameters.put("email", email.getEditText().getText().toString());
+                                parameters.put("passreq", passwordUpdate);
+                                parameters.put("pass", password.getEditText().getText().toString());
+                                return parameters;
+                            }
+                        };
+                        requestQueue.add(request);
+                        Toast.makeText(EditProfile.this, "Profile update successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        //update instructor
+                        StringRequest request = new StringRequest(Request.Method.POST, instUpdateURL, new Response.Listener<String>() {
+                            @Override
+                            @Generated
+                            public void onResponse(String response) {
+                                System.out.println(response);
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            @Generated
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println(error.getMessage());
+                            }
+                        }) {
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> parameters = new HashMap<>();
+                                parameters.put("unum", USER.USER_NUM);
+                                parameters.put("fname", firstName.getEditText().getText().toString());
+                                parameters.put("lname", lastName.getEditText().getText().toString());
+                                parameters.put("email", email.getEditText().getText().toString());
+                                parameters.put("passreq", passwordUpdate);
+                                parameters.put("pass", password.getEditText().getText().toString());
+                                return parameters;
+                            }
+                        };
+                        requestQueue.add(request);
+                        Toast.makeText(EditProfile.this, "Profile update successful", Toast.LENGTH_SHORT).show();
+                    }
                 }
+        }
+    });
 
-                }
-        });
-
-    }
+}
 
     // This function checks if a required text is empty or not
     public boolean isEmpty(TextInputLayout text) {
@@ -169,21 +241,20 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
         if (text.getEditText().getText().toString().isEmpty()) {
             text.setError("Field can't be empty");
             empty = true;
-        }
-        else{
+        } else {
             text.setError(null);
         }
         return empty;
     }
 
     //this function validates email
-    public boolean validateEmail(TextInputLayout text) {
+    public boolean validEmail(TextInputLayout text) {
         boolean valid = true;
 
         String emailInput = text.getEditText().getText().toString().trim();
         if (emailInput.isEmpty()) {
             text.setError("Field can't be empty");
-            valid =  false;
+            valid = false;
         } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(emailInput).matches()) {
             text.setError("Please enter a valid email address");
             valid = false;
@@ -195,28 +266,24 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
     }
 
     //This function validates password only
-    public boolean validatePassword(TextInputLayout password, TextInputLayout confirmPass){
+    public boolean validPassword(TextInputLayout password, TextInputLayout confirmPass) {
         boolean valid = true;
-        if (password.getEditText().getText().toString().isEmpty()){
+        if (password.getEditText().getText().toString().isEmpty()) {
             password.setError("Field can't be empty");
             valid = false;
-        }
-        else if (password.getEditText().getText().toString().length() < 5) {
+        } else if (password.getEditText().getText().toString().length() < 5) {
             password.setError("Password must be at least 5 characters long");
             valid = false;
-        }
-        else{
+        } else {
             password.setError(null);
         }
         if (confirmPass.getEditText().getText().toString().isEmpty()) {
             confirmPass.setError("Confirm your password");
             valid = false;
-        }
-        else if (!password.getEditText().getText().toString().equals(confirmPass.getEditText().getText().toString())) {
+        } else if (!password.getEditText().getText().toString().equals(confirmPass.getEditText().getText().toString())) {
             confirmPass.setError("Passwords don't match");
             valid = false;
-        }
-        else {
+        } else {
             confirmPass.setError(null);
         }
 
@@ -225,20 +292,20 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
 
 
     @Generated
-    private void getStudentData(){
+    private void getStudentData() {
         //Adding the method to the queue by calling the method getTagData
         requestQueue.add(getStudentDataFromServer());
     }
 
     @Generated
-    private JsonArrayRequest getStudentDataFromServer(){
+    private JsonArrayRequest getStudentDataFromServer() {
 
         //Initializing progressbar
-       // final ProgressBar progressBar = (ProgressBar) findViewById(R.id.editProfileProgressBar);
+        // final ProgressBar progressBar = (ProgressBar) findViewById(R.id.editProfileProgressBar);
 
         //Displaying ProgressBar
-       // progressBar.setVisibility(View.VISIBLE);
-       // setProgressBarIndeterminateVisibility(true);
+        // progressBar.setVisibility(View.VISIBLE);
+        // setProgressBarIndeterminateVisibility(true);
 
 
         //JsonArrayRequest of volley
@@ -263,8 +330,8 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
 
     //This method will parse json Data
     @Generated
-    private void parseStudentData(JSONArray array){
-        for (int i = 0; i< array.length(); i++){
+    private void parseStudentData(JSONArray array) {
+        for (int i = 0; i < array.length(); i++) {
 
             JSONObject json = null;
             try {
@@ -275,7 +342,7 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
                 USER.EMAIL = json.getString("Student_Email");
                 email.getEditText().setText(USER.EMAIL);
 
-            } catch (JSONException e){
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
 
@@ -284,20 +351,20 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
     }
 
     @Generated
-    private void getInstructorData(){
+    private void getInstructorData() {
         //Adding the method to the queue by calling the method getTagData
         requestQueue.add(getInstructorDataFromServer());
     }
 
     @Generated
-    private JsonArrayRequest getInstructorDataFromServer(){
+    private JsonArrayRequest getInstructorDataFromServer() {
 
         //Initializing progressbar
         //final ProgressBar progressBar = (ProgressBar) findViewById(R.id.editProfileProgressBar);
 
         //Displaying ProgressBar
         //progressBar.setVisibility(View.VISIBLE);
-       // setProgressBarIndeterminateVisibility(true);
+        // setProgressBarIndeterminateVisibility(true);
 
 
         //JsonArrayRequest of volley
@@ -322,8 +389,8 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
 
     //This method will parse json Data
     @Generated
-    private void parseInstructorData(JSONArray array){
-        for (int i = 0; i< array.length(); i++){
+    private void parseInstructorData(JSONArray array) {
+        for (int i = 0; i < array.length(); i++) {
 
             JSONObject json = null;
             try {
@@ -334,7 +401,7 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
                 USER.EMAIL = json.getString("Instructor_Email");
                 email.getEditText().setText(USER.EMAIL);
 
-            } catch (JSONException e){
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
 
@@ -343,7 +410,7 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
     }
 
     @Generated
-    public void createNewViewDialog(){
+    public void createNewViewDialog() {
         dialogBuilder = new AlertDialog.Builder(this);
         final View viewPopUp = LayoutInflater.from(this)
                 .inflate(R.layout.logout_dialog, null);
@@ -359,7 +426,7 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
             @Override
             @Generated
             public void onClick(View v) {
-                Intent intent5 = new Intent(EditProfile.this,LoginActivity.class);
+                Intent intent5 = new Intent(EditProfile.this, LoginActivity.class);
                 startActivity(intent5);
                 finish();
                 dialog.dismiss();
@@ -380,47 +447,47 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             // STUDENT MENU
-            case R.id.menuHomeStudent :
+            case R.id.menuHomeStudent:
                 Intent intent = new Intent(EditProfile.this, Dashboard.class);
                 startActivity(intent);
                 finish();
                 break;
 
-            case R.id.menuMyCoursesStudent :
+            case R.id.menuMyCoursesStudent:
                 Intent intent2 = new Intent(EditProfile.this, MyCourses.class);
                 startActivity(intent2);
                 finish();
                 break;
 
-            case R.id.menuBrowseCourses :
+            case R.id.menuBrowseCourses:
                 Intent intent1 = new Intent(EditProfile.this, BrowseCourses.class);
                 startActivity(intent1);
                 finish();
                 break;
 
-            case R.id.menuLogOutStudent :
+            case R.id.menuLogOutStudent:
                 createNewViewDialog();
                 break;
 
-            case R.id.menuHomeInstructor :
+            case R.id.menuHomeInstructor:
                 Intent intent5 = new Intent(EditProfile.this, Dashboard.class);
                 startActivity(intent5);
                 finish();
                 break;
 
-            case R.id.menuMyCoursesInstructor :
+            case R.id.menuMyCoursesInstructor:
                 Intent intent3 = new Intent(EditProfile.this, MyCourses.class);
                 startActivity(intent3);
                 finish();
                 break;
 
-            case R.id.menuCreateCourse :
+            case R.id.menuCreateCourse:
                 Intent intent4 = new Intent(EditProfile.this, CreateCourse.class);
                 startActivity(intent4);
                 finish();
                 break;
 
-            case R.id.menuLogOutInstructor :
+            case R.id.menuLogOutInstructor:
                 createNewViewDialog();
                 break;
 
@@ -435,10 +502,10 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
     }
 
     //This method will check if the recyclerview has reached the bottom or not
-    public boolean isLastItemDistplaying(RecyclerView recyclerView){
-        if(recyclerView.getAdapter().getItemCount() != 0){
+    public boolean isLastItemDistplaying(RecyclerView recyclerView) {
+        if (recyclerView.getAdapter().getItemCount() != 0) {
             int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
-            if (lastVisibleItemPosition != RecyclerView.NO_POSITION && lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() -1){
+            if (lastVisibleItemPosition != RecyclerView.NO_POSITION && lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() - 1) {
                 return true;
             }
         }
@@ -449,7 +516,7 @@ public class EditProfile extends AppCompatActivity implements View.OnScrollChang
     @Generated
     public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
         //if Scrolled at last then
-        if(isLastItemDistplaying(recyclerView)){
+        if (isLastItemDistplaying(recyclerView)) {
             //Calling the method getData again
         }
     }
